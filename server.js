c7b3ada3722beb96949995ba83ddbe07eed6468d
puckfried <puckfried@posteo.de>
2022-10-05 14:56:41 +0200

    small

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 5000
 const app = express()
 
 
-app.get('/',(req, res) =>  showAllProducts(req, res)) //controller aufrufen)
+app.get('/', showAllProducts) //controller aufrufen)
 
 
 app.listen(PORT, () => console.log(`Server is running ${PORT}`))
